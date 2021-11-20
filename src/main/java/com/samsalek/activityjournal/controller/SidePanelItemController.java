a76f12f5c0fa420d3b15cb671398910ab39454e0
sam123bir <sam.salek@hotmail.com>
2021-11-20 01:19:35 +0100

    Added side panel items.

---
+++ src/main/java/com/samsalek/activityjournal/controller/SidePanelItemController.java
@@ -0,0 +1,25 @@
+package com.samsalek.activityjournal.controller;
+
+import com.samsalek.activityjournal.model.Month;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class SidePanelItemController extends Controller {
+
+    @FXML Button root;
+
+    Month month;
+
+    public SidePanelItemController(String fxmlName, Pane parent, int nr) {
+        super(fxmlName);
+        this.month = Month.valueOf(nr);
+
+        loadFxml(parent, this);
+    }
+
+    @Override
+    protected void initFXML() {
+        root.setText(month.toString());
+    }
+}
