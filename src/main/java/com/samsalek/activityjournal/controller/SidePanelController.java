8adef0e21429009d6f1eaef7e0a1340267880a1d
sam123bir <sam.salek@hotmail.com>
2021-11-20 01:19:22 +0100

    Added side panel.

--- src/main/java/com/samsalek/activityjournal/controller/SidePanelController.java
+++ src/main/java/com/samsalek/activityjournal/controller/SidePanelController.java
@@ -1,15 +1,51 @@
 package com.samsalek.activityjournal.controller;
 
-class SidePanelController implements Controller {
+import com.samsalek.activityjournal.model.Year;
+import com.samsalek.activityjournal.view.FXMLNames;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
-    String fxmlName;
+import java.util.ArrayList;
 
-    public SidePanelController(String fxmlName) {
-        this.fxmlName = fxmlName;
+class SidePanelController extends Controller {
+
+    @FXML Label yearLabel;
+    @FXML Button previousYearButton;
+    @FXML Button nextYearButton;
+    @FXML VBox monthVBox;
+
+    Year year;
+
+    ArrayList<SidePanelItemController> sidePanelItemControllers;
+
+    public SidePanelController(String fxmlName, Pane parent) {
+        super(fxmlName);
+        year = new Year(2021);
+
+        loadFxml(parent, this);
+
+        sidePanelItemControllers = new ArrayList<>();
+        for(int i = 0; i < 12; i++) {
+            sidePanelItemControllers.add(new SidePanelItemController(FXMLNames.SIDE_PANEL_ITEM, monthVBox, i+1));
+        }
     }
 
     @Override
-    public String getFxmlName() {
-        return fxmlName;
+    public void initFXML() {
+        yearLabel.setText(year.toString());
+        initButtons();
+    }
+
+    public void initButtons() {
+        previousYearButton.setOnAction(actionEvent -> onButtonPress(-1));
+        nextYearButton.setOnAction(actionEvent -> onButtonPress(1));
+    }
+
+    private void onButtonPress(int shift) {
+        year = new Year(year.toInt() + shift);
+        yearLabel.setText(year.toString());
     }
 }
