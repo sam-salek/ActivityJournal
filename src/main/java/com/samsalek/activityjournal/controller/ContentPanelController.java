5a2a3974f48bd0de4e22d5b6ebff9c304f5014de
sam123bir <sam.salek@hotmail.com>
2021-11-23 22:47:03 +0100

    Added a second property panel in the content panel.

--- src/main/java/com/samsalek/activityjournal/controller/ContentPanelController.java
+++ src/main/java/com/samsalek/activityjournal/controller/ContentPanelController.java
@@ -2,6 +2,7 @@ package com.samsalek.activityjournal.controller;
 
 import com.samsalek.activityjournal.util.event.EventHandler;
 import com.samsalek.activityjournal.util.event.Event;
+import com.samsalek.activityjournal.util.event.EventListenerData;
 import com.samsalek.activityjournal.view.FXMLNames;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
@@ -10,28 +11,26 @@ import javafx.scene.layout.Pane;
 public class ContentPanelController extends Controller {
 
     @FXML private AnchorPane rootAnchorPane;
-    @FXML private AnchorPane topPanelAnchorPane;
+    @FXML private AnchorPane firstTopPanelAnchorPane;
+    @FXML private AnchorPane secondTopPanelAnchorPane;
+    @FXML private AnchorPane listAnchorPane;
 
-    private PropertyPanelController propertyPanelController;
+    private PropertyPanelController firstPropertyPanelController;
+    private PropertyPanelController secondPropertyPanelController;
 
     public ContentPanelController(String fxmlName, Pane parent) {
         super(fxmlName, parent);
-
         loadFxmlToParent(parent, this);
-
-        propertyPanelController = new PropertyPanelController(FXMLNames.PROPERTY_PANEL, topPanelAnchorPane, "JANUARY");
-
-        EventHandler.addListener(Event.MonthButtonPress.class, event -> {
-            propertyPanelController.setPanelName(event.getMonth().toString());
-        });
-    }
-
-    @Override
-    protected void initFXML() {
         anchorToParent(rootAnchorPane);
-    }
 
-    public PropertyPanelController getPropertyPanelController() {
-        return propertyPanelController;
+        firstPropertyPanelController = new PropertyPanelController(FXMLNames.PROPERTY_PANEL, firstTopPanelAnchorPane, "2021");
+        secondPropertyPanelController = new PropertyPanelController(FXMLNames.PROPERTY_PANEL, secondTopPanelAnchorPane, "JANUARY");
+
+        EventListenerData test = EventHandler.addListener(Event.YearChange.class, event -> {
+            firstPropertyPanelController.setPanelName(event.getYear().toString());
+        });
+        EventHandler.addListener(Event.MonthButtonPress.class, event -> {
+            secondPropertyPanelController.setPanelName(event.getMonth().toString());
+        });
     }
 }
